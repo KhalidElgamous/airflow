42f0786435b871314b7528d7caa7c97fa0cbf3be
Khalid Elgamous <khalid.elgamous@3wm.ma>
2022-01-17 13:07:09 +0100

    update dag

--- dags/job1.py
+++ dags/job1.py
@@ -5,7 +5,10 @@ from airflow.models.dag import DAG
 
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator, BranchPythonOperator
+
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
@@ -15,6 +18,7 @@ with DAG(
     'task1',
     default_args=default_args,
     description='My first job',
+    start_date=days_ago(0),
     schedule_interval=None,
     catchup=False,
     tags=['scraping'],
